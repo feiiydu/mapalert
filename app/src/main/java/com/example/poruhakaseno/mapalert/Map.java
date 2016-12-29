4b6f3d6d1167d321a2afbc4cca66cec89524fc60
kesano <gun_ini3@hotmail.com>
2016-12-30 01:17:37 +0700

    ปักหมุดได้แล้วววว เลือกรัศมีได้แล้ว

--- app/src/main/java/com/example/poruhakaseno/mapalert/Map.java
+++ app/src/main/java/com/example/poruhakaseno/mapalert/Map.java
@@ -13,10 +13,36 @@ import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import android.app.Dialog;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 public class Map extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    GoogleMap googleMap;
+    LocationManager locationManager;
+    PendingIntent pendingIntent;
+    SharedPreferences sharedPreferences;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +62,83 @@ public class Map extends FragmentActivity implements OnMapReadyCallback {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         dropdown.setAdapter(adapter);
 
+
+
+
+
+
+
     }
+    public  int  returnradious(){
+        Spinner  dropdown = (Spinner)findViewById(R.id.spinner1);
+        int ans;
+        if(dropdown.getSelectedItemPosition()==0){
+            ans = 0;
+            Toast.makeText(getBaseContext(), "Please choose radius.", Toast.LENGTH_SHORT).show();
+        }else if(dropdown.getSelectedItemPosition()==1){
+            ans = 500;
+        }else if(dropdown.getSelectedItemPosition()==2){
+            ans = 1000;
+        }else{
+            ans = 2000;
+        }
+        return ans;
+
+    }
+    @Override
+    public void onMapReady(final GoogleMap googleMap) {
+        LatLng kmitl = new LatLng(13.752092, 100.500893);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kmitl,10.0f));
+        googleMap.setOnMapClickListener(new OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                mMap = googleMap;
+                mMap.clear();
+                Marker mydes = mMap.addMarker(new MarkerOptions().position(latLng).title("My Destination"));
+                if(returnradious()!=0)Toast.makeText(getBaseContext(), "Proximity Alert is added", Toast.LENGTH_SHORT).show();
+                mMap.addCircle(new CircleOptions().center(latLng).radius(returnradious()).fillColor(Color.BLUE).strokeWidth(2).strokeColor(Color.BLACK));
+            }
+        });
+
+    }
+
+
+    private void drawMarker(LatLng point){
+        // Creating an instance of MarkerOptions
+        MarkerOptions markerOptions = new MarkerOptions();
+
+        // Setting latitude and longitude for the marker
+        markerOptions.position(point);
+
+        // Adding marker on the Google Map
+        googleMap.addMarker(markerOptions);
+    }
+
+    private void drawCircle(LatLng point){
+
+        // Instantiating CircleOptions to draw a circle around the marker
+        CircleOptions circleOptions = new CircleOptions();
+
+        // Specifying the center of the circle
+        circleOptions.center(point);
+
+        // Radius of the circle
+        circleOptions.radius(20);
+
+        // Border color of the circle
+        circleOptions.strokeColor(Color.BLACK);
+
+        // Fill color of the circle
+        circleOptions.fillColor(0x30ff0000);
+
+        // Border width of the circle
+        circleOptions.strokeWidth(2);
+
+        // Adding the circle to the GoogleMap
+        googleMap.addCircle(circleOptions);
+
+    }
+
 
 
     /**
@@ -48,15 +150,6 @@ public class Map extends FragmentActivity implements OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
 
 
-        // Add a marker in Sydney and move the camera
-
-        LatLng kmitl = new LatLng(13.727876, 100.772365);
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kmitl,17.0f));
-        Marker KMITL = mMap.addMarker(new MarkerOptions().position(kmitl).draggable(true).title("Drag to your destination"));
-    }
 }
